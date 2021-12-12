e5131f9e1c3fa51213ae3b3cd1e5769ae1c0d9a0
Nguyen Ngoc Duc <nguyenngocduc.tosa@gmail.com>
2021-12-12 14:37:15 +0700

    By Ngoc Duc
    Thêm Register SV, xml,java class sửa đổi mainfest

---
+++ app/src/main/java/com/example/ltnc20211/sinhvien/RegisterSVActivity.java
@@ -0,0 +1,16 @@
+package com.example.ltnc20211.sinhvien;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ltnc20211.R;
+
+public class RegisterSVActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_svactivity);
+    }
+}
